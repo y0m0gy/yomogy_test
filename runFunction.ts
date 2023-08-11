14a6585823d4c90fb3f774961b2e4e5fda96a06e
y0m0gy <yomogy.info@gmail.com>
2023-08-11 19:39:49 +0900

    fix github action

--- runFunction.ts
+++ runFunction.ts
@@ -1,11 +1,9 @@
 import { createJsonForAuthorsAndPosts } from "./src/pages/api/get-posts-category";
-// import { createImage } from "./src/pages/api/make-fig";
 import { createRssFeed } from "./src/pages/api/rss";
 import { createSitemap } from "./src/pages/api/sitemap";
 
 async function run() {
   await createJsonForAuthorsAndPosts();
-  // await createImage();
   await createRssFeed();
   await createSitemap();
 }
