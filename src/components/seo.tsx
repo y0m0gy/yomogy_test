07915ddb3139fd21c5493ba2bf2ae1805b45823a
y0m0gy <yomogy.info@gmail.com>
2023-08-12 14:20:12 +0900

    fix org

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -31,19 +31,34 @@ const Seo: React.FC<MetaData> = ({
 
   return (
     <Head>
-      <title>{title}</title>
-      <meta name="viewport" content="width=device-width,initial-scale=1.0" />
-      <meta name="description" content={description} />
-      <meta property="og:url" content={url} />
-      <meta property="og:title" content={title} />
-      <meta property="og:site_name" content={title} />
-      <meta property="og:description" content={description} />
-      <meta property="og:type" content="website" />
-      <meta property="og:image" content={imgUrl} />
-      <meta property="og:image:width" content={String(imgWidth)} />
-      <meta property="og:image:height" content={String(imgHeight)} />
-
-      <link rel="canonical" href={url} />
+      <title key="pageTitle">{title}</title>
+      <meta
+        key="metaViewport"
+        name="viewport"
+        content="width=device-width,initial-scale=1.0"
+      />
+      <meta key="metaDescription" name="description" content={description} />
+      <meta key="ogUrl" property="og:url" content={url} />
+      <meta key="ogTitle" property="og:title" content={title} />
+      <meta key="ogSiteName" property="og:site_name" content={title} />
+      <meta
+        key="ogDescription"
+        property="og:description"
+        content={description}
+      />
+      <meta key="ogType" property="og:type" content="website" />
+      <meta key="ogImage" property="og:image" content={imgUrl} />
+      <meta
+        key="ogImageWidth"
+        property="og:image:width"
+        content={String(imgWidth)}
+      />
+      <meta
+        key="ogImageHeight"
+        property="og:image:height"
+        content={String(imgHeight)}
+      />
+      <link key="canonicalLink" rel="canonical" href={url} />
     </Head>
   );
 };
