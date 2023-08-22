df251f8534a0ba7cb5f8156af8e70d9f8b3fabf2
y0m0gy <yomogy.info@gmail.com>
2023-08-22 10:04:33 +0900

    pre app page

---
+++ src/pages/app/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Link from "next/link";
+
+const MainPage: React.FC = () => {
+  // 例としてのダミーデータ
+  const items = ["editor"];
+
+  return (
+    <div className="min-h-screen p-4">
+      <h1 className="text-2xl font-bold mb-4">Main Page</h1>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index} className="mb-2">
+            <Link
+              href={`/app/${item}`}
+              className="text-blue-500 hover:underline"
+            >
+              {item}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default MainPage;
