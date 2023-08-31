ec27391687635c8a5b1da89a6defcc1e7f0b0bef
y0m0gy <yomogy.info@gmail.com>
2023-08-31 16:50:17 +0900

    add legal page

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -12,7 +12,7 @@ export async function getStaticProps() {
   };
 }
 
-export default function Contact({
+export default function Administrator({
   newPosts,
   recommendPosts,
 }: {
@@ -25,6 +25,48 @@ export default function Contact({
         <div className="blog_main bg-white dark:bg-gray-900 p-4 lg:p-8 max-w-6xl mx-auto w-full max-w-full">
           <>
             {/* 以下の部分のみ動的に変更すると静的ページテンプレとして使用できる */}
+            <h1 className="text-2xl font-bold mb-4">運営者</h1>
+
+            <p className="mt-4">
+              記事に関するお問い合わせや、その他何かご要望や不明点などございましたら、メール等にてご連絡ください。
+            </p>
+
+            <h2 className="text-xl font-semibold mt-4">お問い合わせ情報</h2>
+
+            <h3 className="mt-4">連絡先</h3>
+            <p className="mt-2">
+              Mail :&nbsp;
+              <a
+                href="mailto:yomogy.info@gmail.com"
+                className="text-blue-500 underline"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                yomogy.info [at] gmail.com
+              </a>
+            </p>
+            <p className="mt-2">
+              Github :&nbsp;
+              <a
+                href="https://github.com/yomogyhub"
+                className="text-blue-500 underline"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                yomogyhub
+              </a>
+            </p>
+            <p className="mt-2">
+              X (Twitter) :&nbsp;
+              <a
+                href="https://x.com/y0m0gy"
+                className="text-blue-500 underline"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                @y0m0gy
+              </a>
+            </p>
 
             {/* ここまで */}
           </>
